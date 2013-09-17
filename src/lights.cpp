5d2d942b86418bbbd4c63534a951c4828f7493f6
Sean Connell <sean@seanconnell.net>
2013-09-16 19:21:23 -0700

    Finished migration to structs. works

--- src/lights.cpp
+++ src/lights.cpp
@@ -1,5 +1,5 @@
 /*
- * Code to control an RGB LED strip. Meant to be used for my bicycle 
+ * Code to control an RGB LED strip.
  * lighting project.
  * 
  * Author: Sean Connell, based on sample code from Nate @ Sparkfun
@@ -12,13 +12,12 @@
 #include "lights.h"
 #include <SPI.h>
 
-//Represents the state of each color on each RGB LED
-COLOR * strip_colors[STRIP_LENGTH];
+
 //colors offset by 120 degrees
 ITERATOR red_itr = { 1, 0 };
 ITERATOR grn_itr = { 1, 333 };
 ITERATOR blu_itr = { 1, 666 };
-ITERATOR dim_itr = { 7, 231 };
+ITERATOR dim_itr = { 2, 231 };
 ITERATOR dly_itr = { 10, 0 };
 
 void hard_spi_init(){
@@ -30,41 +29,46 @@ void hard_spi_init(){
 }
 
 //Iterate a data array in a circle
-uint8_t get_next_value(ITERATOR itr, TABLE values){
-	itr.loc = itr.loc + itr.step;
-	if(itr.loc >= values.length){
-		itr.loc = itr.loc - values.length;
+uint8_t get_next_value(ITERATOR * itr, TABLE * values){
+	itr->loc = itr->loc + itr->step_size;
+	if(itr->loc >= values->length){
+		itr->loc = itr->loc - values->length;
 	}
-	return values.data[itr.loc]; 
+	return values->data[itr->loc]; 
 }
 
 //mutate color to be dimmed by a mask value 
 void dim_color(COLOR * color){
-	uint8_t mask = get_next_value(dim_itr, triangle_table);
+	uint8_t mask = get_next_value(&dim_itr, &triangle_table);
 	color->red &= mask;
 	color->grn &= mask;
 	color->blu &= mask;
 }
 
+void copy_color(COLOR * from, COLOR * to){
+       to->red = from->red;
+       to->grn = from->grn;
+       to->blu = from->blu; 
+}
+
 void increment_frame(void) {
     //First, move all the current colors down one spot on the strip
     for(uint8_t x = (STRIP_LENGTH -1); x > 0; x--){
-	strip_colors[x-1] = strip_colors[x];
+        copy_color(&strip_colors[x-1], &strip_colors[x]);
     }
-    //Add new color to the head of the queue
-    COLOR * next_color = strip_colors[0];
-    next_color->red = get_next_value(red_itr, sine_table);
-    next_color->grn = get_next_value(grn_itr, sine_table);
-    next_color->blu = get_next_value(blu_itr, sine_table);
-    //dim_color(strip_colors[0]);//apply dimming effect
+    COLOR * next_color = &strip_colors[0];
+    next_color->red = get_next_value(&red_itr, &sine_table);
+    next_color->grn = get_next_value(&grn_itr, &sine_table);
+    next_color->blu = get_next_value(&blu_itr, &sine_table);
+    dim_color(next_color);//apply dimming effect
 }
 
 void push_frame(void) {
     //Send all values of LEDs in order
     for(int LED_number = 0; LED_number < STRIP_LENGTH; LED_number++) {
-	SPI.transfer(strip_colors[LED_number]->red);
-	SPI.transfer(strip_colors[LED_number]->grn);
-	SPI.transfer(strip_colors[LED_number]->blu);
+	SPI.transfer(strip_colors[LED_number].red);
+	SPI.transfer(strip_colors[LED_number].grn);
+	SPI.transfer(strip_colors[LED_number].blu);
     }
     //Pull clock low to put strip into reset/post mode
     digitalWrite(CKI, LOW);
@@ -73,17 +77,12 @@ void push_frame(void) {
 
 void setup(){
 	hard_spi_init();
-	for(uint8_t i=0; i<STRIP_LENGTH; i++){
-		strip_colors[i] = &((COLOR){0,150,0});
-	}
-
 }
 
 void loop(){
     while(WORLD_EXISTS){ 
-        //increment_frame(); // get new data and setup buffer with next state
+        increment_frame(); // get new data and setup buffer with next state
         push_frame(); //update display to reflect new buffer state
-	delay(100);
-        //delay(get_next_value(dly_itr, triangle_table)>>2);//adjusts update rate
+        delay((get_next_value(&dly_itr, &triangle_table)>>3) + 5);//adjusts update rate
     }
 }
