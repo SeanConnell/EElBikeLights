fa691adc679a6855635918f430ba74768562f1c4
Sean Connell <sean@seanconnell.net>
2013-09-22 20:35:45 -0700

    Added another echo handler to make debugging easier

--- src/command_dispatcher.cpp
+++ src/command_dispatcher.cpp
@@ -9,14 +9,23 @@ void unrecognized(){
   Serial.println("What?");
 }
 
-void initialize_handlers(){//Needs to have serial started beforehand
-	SCmd.addCommand("RESET", reset);
-	SCmd.addDefaultHandler(unrecognized);  
+void echo(){
+	char *arg;  
+	Serial.print("ECHO "); 
+	arg = SCmd.next();    // Get the next argument from the SerialCommand object buffer
+	while (arg != NULL){     // As long as it exists, take it 
+		Serial.print(arg); 
+		arg = SCmd.next();    // Get the next argument from the SerialCommand object buffer
+	} 
+	Serial.print('\n'); //print newline to end this
 }
 
 void handle_data(){//dispatch to handlers once data is recieved
 	SCmd.readSerial();
 }
 
-
-
+void initialize_handlers(){//Needs to have serial started beforehand
+	SCmd.addCommand("RESET", reset);
+	SCmd.addCommand("ECHO", echo);
+	SCmd.addDefaultHandler(unrecognized);  
+}
